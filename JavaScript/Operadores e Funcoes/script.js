ec9ece3f7be950f74051ddc2cb9c352cbc07c446
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2026-01-29 08:46:18 -0300

    JavaScript

---
+++ JavaScript/Operadores e Funcoes/script.js
@@ -0,0 +1,15 @@
+console.log("Olá, JavaScript!");
+
+let c = 3;
+let d = 6;
+let e = 8;
+console.log((c + d + e)/3);
+console.log(c%d);
+
+let x = 10;
+let y = "10";
+
+console.log(x == y); // igual (valor)
+console.log(x === y); // igual (valor e tipo)
+console.log(x != y); // diferente (valor)
+console.log(x !== y); // diferente(valor e tipo)
