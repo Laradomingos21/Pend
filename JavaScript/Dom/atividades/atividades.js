ac5dda35fedb20aa9ed3383fc9699d84475f0a36
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2026-03-03 15:59:31 -0300

    Atividades

---
+++ JavaScript/Dom/atividades/atividades.js
@@ -0,0 +1,14 @@
+//Atividade 1
+let textoo = document.querySelector(".textoo");
+let bota0 = document.querySelector("#bota0");
+let botao = document.querySelector("#botao");
+
+bota0.addEventListener("click", function() {
+    textoo.style.color = "blue"; 
+});
+
+botao.addEventListener("click", function() {
+    textoo.style.color = "red";
+});
+
+//Atividade 2
