8457cca4d3af9d184eeb7a60612a93f804916364
Laradomingos21 <lara.v.domingos@aluno.senai.br>
2026-08-13 14:11:24 -0300

    Orientacao e Objeto

--- JavaScript/Orientacao a Objeto/Sistema de cadastro - parte 2/script.js
+++ JavaScript/Orientacao a Objeto/Sistema de cadastro - parte 2/script.js
@@ -7,29 +7,42 @@ class Cadastro {
     }
 
     aplicarDesconto() {
-        const ProdutoDesconto = preco * (1 - (desconto / 100));
+        this.preco = this.preco - (this.preco *this.desconto / 100);
+
+        return this.preco;
+    }
+}
+
+class Gerente {
+    constructor() {
+        this.Gerente = [];
+    }
+
+    adicionarProduto(Cadastro) {
+        this.Gerente.push(Cadastro);
     }
 
     exibir() {
+
         const resultado = document.querySelector("#resultado");
 
         resultado.innerHTML = "";
 
-        this.cadastros.forEach(cadastro => {
+        this.Gerente.forEach(cadastro => {
             resultado.innerHTML += `
             <div>
                 <p>Nome: ${cadastro.nome}</p>
-                <p>Idade: ${cadastro.idade}</p>
-                <p>Curso: ${cadastro.curso}</p>
-                <p>Matricula: ${cadastro.matricula}</p>
+                <p>Preço: ${cadastro.aplicarDesconto(cadastro)}</p>
+                <p>Categoria: ${cadastro.categoria}</p>
+                <p>Desconto: ${cadastro.desconto}%</p>
 
                 ----------------------
             </div>`;
-            });
-        }
+        });
     } 
+}
 
-// const cadastro = new Cadastro();
+const gerente = new Gerente();
 const nome = document.querySelector("#nome");
 const preco = document.querySelector("#preco");
 const categoria = document.querySelector("#categoria");
@@ -38,12 +51,8 @@ const botaoCadastrar = document.querySelector("#botaoCadastrar");
 
 botaoCadastrar.addEventListener("click", function() {
   
-    const aluno = new Cadastro(nome.value, preco.value, categoria.value, desconto.value);
+    const produto = new Cadastro(nome.value, preco.value, categoria.value, desconto.value);
 
-    cadastro.aplicarDesconto(cadastro);
-    cadastro.exibir();
-
+    gerente.adicionarProduto(produto);
+    gerente.exibir();
 });
-
-
-
